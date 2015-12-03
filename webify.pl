d56f4716c4cb561cf9aa8bfc2d5c7792cd978678
Franco Fichtner <franco@opnsense.org>
2015-12-03 20:57:07 +0100

    trusty old perl already does half of the webify...

---
+++ webify.pl
@@ -0,0 +1,82 @@
+#!/usr/bin/env perl
+
+# Copyright (c) 2015 Franco Fichtner <franco@opnsense.org>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+use strict;
+use warnings;
+use autodie;
+
+my $ver = shift;
+my @lines;
+my $doc;
+
+open $doc, "<", "doc/" . $ver;
+
+while (<$doc>) { push @lines, $_; }
+
+my $date;
+my %refs;
+
+# first pass: read date, build table, strip stuff
+for (@lines) {
+	chomp;
+	$refs{$1} = $2 if $_ =~ s/^\[(\d*?)\]\s*(.*)\s*$//g;
+	$date = $1 if $_ =~ s/^@\s*(.*)\s*$//g;
+	$_ =~ s/^--.*$//g;
+	$_ =~ s/^\s*$//g;
+}
+
+# print json metadata to standard error
+say STDERR "{version: \"$ver\", date: \"$date\"}";
+
+my $skip = 0;
+my $pp = '<p>';
+
+# second pass: webify all the text!
+for (@lines) {
+	if ( $_ eq '' ) {
+		$skip = 1;
+		next;
+	}
+
+	$_ =~ s/(http[s]?:\/\/\S*)/<a href="$1">$1<\/a>/g;
+	foreach my $key ( keys %refs ) {
+		$_ =~ s/\[$key\]/[<a href="$refs{$key}">$key<\/a>]/g;
+	}
+
+	if ( $pp ne '<p>' ) {
+		if ( $skip ) {
+			print STDOUT $pp;
+			print STDOUT "</p>\n";
+			$skip = 0;
+			$pp = '<p>' . $_;
+			next;
+		}
+		$pp .= ' ';
+	}
+
+	$pp .= $_;
+}
