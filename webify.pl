b4d7fc8d231f4c47c7d6ff4022a10d621935b690
Franco Fichtner <franco@opnsense.org>
2015-12-17 19:53:11 +0100

    webify: annotate the script

--- webify.pl
+++ webify.pl
@@ -40,41 +40,56 @@ while (<$doc>) { push @lines, $_; }
 my $date;
 my %refs;
 
-# first pass: read date, build table, strip stuff
+# first pass: sanitise input
 for ( @lines ) {
+	# remove newline
 	chomp;
+	# collect links
 	$refs{$1} = $2 if $_ =~ s/^\[(\d*?)\]\s*(.*)\s*$//g;
+	# collect date
 	$date = $1 if $_ =~ s/^@\s*(.*)\s*$//g;
+	# remove comment lines
 	$_ =~ s/^--.*$//g;
-	$_ =~ s/\s*$//g;
+	# remove trailing noise
+	$_ =~ s/\s+$//g;
 }
 
 # print json metadata to standard error
 say STDERR "{version: \"$ver\", date: \"$date\"}";
 
+# initialise tag states for:
+# paragraph
 my $pp = '<p>';
+# bullet list
 my $bl = '';
+# literals
 my $li = '';
-my $in = 0;
 
+# add empty line to flush paragaph
 push @lines, '';
 
 # second pass: webify all the text!
 for ( @lines ) {
+	# transform literal URLs into hyperlinks
 	$_ =~ s/(http[s]?:\/\/\S*)/<a target="_blank" href="$1">$1<\/a>/g;
+
+	# transform references into hyperlinks
 	foreach my $key ( keys %refs ) {
 		$_ =~ s/\[$key\]/[<a target="_blank" href="$refs{$key}">$key<\/a>]/g;
 	}
 
+	# bullet list continuation
 	if ( $bl ne '' && $_ =~ s/^\s+//g ) {
 		$pp =~ s/<\/li>\n$/ /g;
 	}
 
+	# literal block transformation
 	if ( $_ =~ s/^#\s//g ) {
 		if ( $li eq '' ) {
 			$li = '</pre>';
 			$pp .= '<pre>';
 		}
+	# bullet list transformation
 	} elsif ( $_ =~ s/^o\s+//g ) {
 		if ( $bl eq '' ) {
 			$bl = '</ul>';
@@ -83,23 +98,33 @@ for ( @lines ) {
 		$pp .= '<li>';
 	}
 
+	# ignores empty lines
 	if ( $pp ne '<p>' ) {
+		# end of the paragraph
 		if ( $_ eq '' ) {
+			# adjust last newline for literal block end
 			chop $pp if $li ne '';
+			# print paragraph trailer
 			print STDOUT $pp . $li . $bl . '</p>' . "\n";
+			# reset tag states
 			$pp = '<p>';
 			$bl = '';
 			$li = '';
+		# text continuation
 		} else {
+			# literal block embedded newline
 			if ( $li ne '' ) {
 				$_ .= "\n";
+			# bullet list list item end
 			} elsif ( $bl ne '' ) {
 				$_ .= "</li>\n";
+			# normal text append
 			} else {
 				$pp .= ' ';
 			}
 		}
 	}
 
+	# merge the transformed line
 	$pp .= $_;
 }
